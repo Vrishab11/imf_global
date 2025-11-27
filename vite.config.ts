dc626bbe881ff9fa303b2f5de398d74f53b3a01d
Vrishab N <vrishabnalothayyil@gmail.com>
2025-11-27 20:26:19 +0530

    Deploy mobile edits

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  // base: process.env.VITE_BASE_PATH || '/imf_global',
+  base: process.env.VITE_BASE_PATH || '/imf_global',
 })
